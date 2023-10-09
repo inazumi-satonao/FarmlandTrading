4bcf96d5836dd8fcec035a275ee1e62a43bab59c
inazumi-satonao <139128891+inazumi-satonao@users.noreply.github.com>
2023-10-10 02:04:11 +0900

    10.9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,37 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @title = params[:title]
+    if @title.present?
+      @posts = Post.where('title LIKE ?', "%#{@title}%")
+    else
+      @posts = Post.all
+    end
+    render :index
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if params[:post][:image]
+      @post.image.attach(params[:post][:image])
+    end
+
+    if @post.save
+      redirect_to index_post_path, notice: '登録しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
+  end
 end
